aad5c2a41b283b8153ec006556fa358171462750
Chinedum Echeta <60179183+cecheta@users.noreply.github.com>
2024-01-16 17:13:36 +0000

    Add config for GPG commit signing

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -e
 
+# GPG commit signing
+sudo apt-get update && sudo apt-get install gnupg2 -y
+
+# Add to ~/.zshrc
 {
     # AZ CLI autocomplete
     echo "[[ -f /etc/bash_completion.d/azure-cli ]] && source /etc/bash_completion.d/azure-cli"
