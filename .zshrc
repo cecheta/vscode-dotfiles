8de555aeda2dc83ea009e035839582c0451f4a17
Chinedum Echeta <60179183+cecheta@users.noreply.github.com>
2025-12-16 11:37:47 +0000

    Add uv completion

--- .zshrc
+++ .zshrc
@@ -1,7 +1,2 @@
 # AZ CLI completion
 [[ -f /etc/bash_completion.d/azure-cli ]] && source /etc/bash_completion.d/azure-cli || :
-
-# azd completion
-if [[ $(command -v azd) && ! -f /usr/local/share/zsh/site-functions/_azd ]]; then
-    azd completion zsh | sudo tee /usr/local/share/zsh/site-functions/_azd >/dev/null 
-fi
--- install.sh
+++ install.sh
@@ -3,6 +3,16 @@
 # GPG commit signing
 sudo apt-get update && sudo apt-get install gnupg2 -y
 
+# azd completion
+if [[ $(command -v azd) ]]; then
+    azd completion zsh | sudo tee /usr/local/share/zsh/site-functions/_azd >/dev/null
+fi
+
+# uv completion
+if [[ $(command -v uv) ]]; then
+    uv generate-shell-completion zsh | sudo tee /usr/local/share/zsh/site-functions/_uv > /dev/null
+fi
+
 # Add to ~/.zshrc
 cat .zshrc >> ~/.zshrc
 
