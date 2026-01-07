ed7e93e55d81f8e9007fc430b8278ab9035a1ac0
Chinedum Echeta <60179183+cecheta@users.noreply.github.com>
2026-01-07 18:49:36 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,11 @@ if command -v azd > /dev/null 2>&1; then
     azd completion zsh | sudo tee /usr/local/share/zsh/site-functions/_azd > /dev/null
 fi
 
+# gh completion
+if command -v gh > /dev/null 2>&1; then
+    gh completion -s zsh | sudo tee /usr/local/share/zsh/site-functions/_gh > /dev/null
+fi
+
 # uv completion
 if command -v uv > /dev/null 2>&1; then
     uv generate-shell-completion zsh | sudo tee /usr/local/share/zsh/site-functions/_uv > /dev/null
